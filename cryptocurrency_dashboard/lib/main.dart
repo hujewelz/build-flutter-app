ba2a212ae4db53460d0cbe0dc461f1a1fd04421b
Jewelz <hujewelz@163.com>
2020-03-19 23:32:20 +0800

    set dark theme

--- cryptocurrency_dashboard/lib/main.dart
+++ cryptocurrency_dashboard/lib/main.dart
@@ -7,23 +7,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // return CupertinoApp(
-    //   color: Colors.redAccent,
-    //   theme: CupertinoThemeData(
-    //     brightness: Brightness.dark,
-    //     primaryColor: Colors.red,
-    //     primaryContrastingColor: Colors.redAccent,
-    //     barBackgroundColor: CupertinoColors.darkBackgroundGray,
-    //     scaffoldBackgroundColor: Colors.blueGrey,
-    //   ),
-    //   home: HomeScreen(),
-    // );
     return MaterialApp(
       title: 'Pryptocurrency Dashboard',
       color: Color(0xFFF9F8FA),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
       theme: ThemeData(
         platform: TargetPlatform.macOS,
         primaryColor: Color(0xFFF9F8FA),
